cart/bevy

pipelined rendering proof of concept

pipelined/bevy_render2/src/pass/pass.rs
@@ -1,0 +1,56 @@
+use super::Operations;
+use crate::{color::Color, render_resource::TextureId};
+
+#[derive(Debug, Clone)]
+pub enum TextureAttachment {
+    Id(TextureId),
+    Input(String),
+}
+
+impl TextureAttachment {
+    pub fn get_texture_id(&self) -> Option<TextureId> {
+        if let TextureAttachment::Id(texture_id) = self {
+            Some(*texture_id)
+        } else {
+            None
+        }
+    }
+}
+
+#[derive(Clone, Debug)]
+pub struct ClearColor(pub Color);
+
+impl Default for ClearColor {
+    fn default() -> Self {
+        Self(Color::rgb(0.4, 0.4, 0.4))
+    }
+}
+
+#[derive(Debug, Clone)]
+pub struct RenderPassColorAttachmentDescriptor {
+    /// The actual color attachment.
+    pub attachment: TextureAttachment,
+
+    /// The resolve target for this color attachment, if any.
+    pub resolve_target: Option<TextureAttachment>,
+
+    /// What operations will be performed on this color attachment.
+    pub ops: Operations<Color>,
+}
+
+#[derive(Debug, Clone)]
+pub struct RenderPassDepthStencilAttachmentDescriptor {
+    pub attachment: TextureAttachment,
+    /// What operations will be performed on the depth part of the attachment.
+    pub depth_ops: Option<Operations<f32>>,
+    /// What operations will be performed on the stencil part of the attachment.
+    pub stencil_ops: Option<Operations<u32>>,
+}
+
+// A set of pipeline bindings and draw calls with color and depth outputs
+#[derive(Debug, Clone)]
+pub struct PassDescriptor {
+    pub color_attachments: Vec<RenderPassColorAttachmentDescriptor>,
+    pub depth_stencil_attachment: Option<RenderPassDepthStencilAttachmentDescriptor>,
+    pub sample_count: u32,
+}

pipelined/bevy_render2/src/render_graph/nodes/window_swap_chain_node.rs
@@ -1,0 +1,99 @@
+use crate::{
+    render_graph::{Node, ResourceSlotInfo, ResourceSlots},
+    render_resource::{RenderResourceId, RenderResourceType, SwapChainDescriptor},
+    renderer::RenderContext,
+};
+use bevy_ecs::world::World;
+use bevy_utils::HashMap;
+use bevy_window::WindowId;
+use raw_window_handle::{HasRawWindowHandle, RawWindowHandle};
+use std::borrow::Cow;
+use std::ops::{Deref, DerefMut};
+
+pub struct ExtractedWindow {
+    pub id: WindowId,
+    pub handle: RawWindowHandleWrapper,
+    pub physical_width: u32,
+    pub physical_height: u32,
+    pub vsync: bool,
+}
+
+#[derive(Default)]
+pub struct ExtractedWindows {
+    pub windows: HashMap<WindowId, ExtractedWindow>,
+}
+
+impl Deref for ExtractedWindows {
+    type Target = HashMap<WindowId, ExtractedWindow>;
+
+    fn deref(&self) -> &Self::Target {
+        &self.windows
+    }
+}
+
+impl DerefMut for ExtractedWindows {
+    fn deref_mut(&mut self) -> &mut Self::Target {
+        &mut self.windows
+    }
+}
+
+pub struct RawWindowHandleWrapper(pub RawWindowHandle);
+
+// TODO: THIS IS NOT SAFE ... ONLY FOR PROTOTYPING
+unsafe impl Send for RawWindowHandleWrapper {}
+unsafe impl Sync for RawWindowHandleWrapper {}
+
+// TODO: safe?
+unsafe impl HasRawWindowHandle for RawWindowHandleWrapper {
+    fn raw_window_handle(&self) -> RawWindowHandle {
+        self.0.clone()
+    }
+}
+
+pub struct WindowSwapChainNode {
+    window_id: WindowId,
+}
+
+impl WindowSwapChainNode {
+    pub const OUT_TEXTURE: &'static str = "texture";
+
+    pub fn new(window_id: WindowId) -> Self {
+        WindowSwapChainNode { window_id }
+    }
+}
+
+impl Node for WindowSwapChainNode {
+    fn output(&self) -> &[ResourceSlotInfo] {
+        static OUTPUT: &[ResourceSlotInfo] = &[ResourceSlotInfo {
+            name: Cow::Borrowed(WindowSwapChainNode::OUT_TEXTURE),
+            resource_type: RenderResourceType::Texture,
+        }];
+        OUTPUT
+    }
+
+    fn update(
+        &mut self,
+        world: &World,
+        render_context: &mut dyn RenderContext,
+        _input: &ResourceSlots,
+        output: &mut ResourceSlots,
+    ) {
+        let windows = world.get_resource::<ExtractedWindows>().unwrap();
+        let window = windows
+            .get(&self.window_id)
+            .expect("Window swapchain node refers to a non-existent window.");
+
+        let render_resource_context = render_context.resources_mut();
+        let swap_chain_descriptor = SwapChainDescriptor {
+            window_id: window.id,
+            format: crate::texture::TextureFormat::Bgra8UnormSrgb,
+            width: window.physical_width,
+            height: window.physical_height,
+            vsync: window.vsync,
+        };
+
+        let swap_chain_texture =
+            render_resource_context.next_swap_chain_texture_v2(&swap_chain_descriptor);
+        output.set(0, RenderResourceId::Texture(swap_chain_texture));
+    }
+}

pipelined/bevy_render2/src/render_resource/buffer_vec.rs
@@ -1,0 +1,124 @@
+use crate::{
+    render_resource::{BufferId, BufferInfo, BufferMapMode, BufferUsage},
+    renderer::{RenderContext, RenderResources},
+};
+use bevy_core::{Byteable, Bytes};
+
+// TODO: replace with Pod / Zeroable
+pub struct BufferVec<T: Byteable> {
+    values: Vec<T>,
+    staging_buffer: Option<BufferId>,
+    buffer: Option<BufferId>,
+    capacity: usize,
+    item_size: usize,
+    buffer_usage: BufferUsage,
+}
+
+impl<T: Byteable> Default for BufferVec<T> {
+    fn default() -> Self {
+        Self {
+            values: Vec::new(),
+            staging_buffer: None,
+            buffer: None,
+            capacity: 0,
+            buffer_usage: BufferUsage::all(),
+            item_size: std::mem::size_of::<T>(),
+        }
+    }
+}
+
+impl<T: Byteable> BufferVec<T> {
+    pub fn new(buffer_usage: BufferUsage) -> Self {
+        Self {
+            buffer_usage,
+            ..Default::default()
+        }
+    }
+    #[inline]
+    pub fn staging_buffer(&self) -> Option<BufferId> {
+        self.staging_buffer
+    }
+
+    #[inline]
+    pub fn buffer(&self) -> Option<BufferId> {
+        self.buffer
+    }
+
+    #[inline]
+    pub fn capacity(&self) -> usize {
+        self.capacity
+    }
+
+    pub fn push(&mut self, value: T) -> usize {
+        if self.values.len() < self.capacity {
+            let index = self.values.len();
+            self.values.push(value);
+            index
+        } else {
+            panic!(
+                "Cannot push value because capacity of {} has been reached",
+                self.capacity
+            );
+        }
+    }
+
+    pub fn reserve(&mut self, capacity: usize, render_resources: &RenderResources) {
+        if capacity > self.capacity {
+            self.capacity = capacity;
+            if let Some(staging_buffer) = self.staging_buffer.take() {
+                render_resources.remove_buffer(staging_buffer);
+            }
+
+            if let Some(buffer) = self.buffer.take() {
+                render_resources.remove_buffer(buffer);
+            }
+
+            let size = self.item_size * capacity;
+            self.staging_buffer = Some(render_resources.create_buffer(BufferInfo {
+                size,
+                buffer_usage: BufferUsage::COPY_SRC | BufferUsage::MAP_WRITE,
+                mapped_at_creation: false,
+            }));
+            self.buffer = Some(render_resources.create_buffer(BufferInfo {
+                size,
+                buffer_usage: BufferUsage::COPY_DST | self.buffer_usage,
+                mapped_at_creation: false,
+            }));
+        }
+    }
+
+    pub fn reserve_and_clear(&mut self, capacity: usize, render_resources: &RenderResources) {
+        self.clear();
+        self.reserve(capacity, render_resources);
+    }
+
+    pub fn write_to_staging_buffer(&self, render_resources: &RenderResources) {
+        if let Some(staging_buffer) = self.staging_buffer {
+            let size = self.values.len() * self.item_size;
+            render_resources.map_buffer(staging_buffer, BufferMapMode::Write);
+            render_resources.write_mapped_buffer(
+                staging_buffer,
+                0..size as u64,
+                &mut |data, _renderer| {
+                    self.values.write_bytes(data);
+                },
+            );
+            render_resources.unmap_buffer(staging_buffer);
+        }
+    }
+    pub fn write_to_uniform_buffer(&self, render_context: &mut dyn RenderContext) {
+        if let (Some(staging_buffer), Some(uniform_buffer)) = (self.staging_buffer, self.buffer) {
+            render_context.copy_buffer_to_buffer(
+                staging_buffer,
+                0,
+                uniform_buffer,
+                0,
+                (self.values.len() * self.item_size) as u64,
+            );
+        }
+    }
+
+    pub fn clear(&mut self) {
+        self.values.clear();
+    }
+}

astrozac81/bery

pipelined/bevy_render2/src/pass/pass.rs
@@ -1,0 +1,56 @@
+use crate::{color::Color, pass::Operations, render_resource::TextureId};
+
+
+#[derive(Debug, Clone)]
+pub enum TextureAttachment {
+    Id(TextureId),
+    Input(String),
+}
+
+impl TextureAttachment {
+    pub fn get_texture_id(&self) -> Option<TextureId> {
+        if let TextureAttachment::Id(texture_id) = self {
+            Some(*texture_id)
+        } else {
+            None
+        }
+    }
+}
+
+#[derive(Clone, Debug)]
+pub struct ClearColor(pub Color);
+
+impl Default for ClearColor {
+    fn default() -> Self {
+        Self(Color::rgb(0.4, 0.4, 0.4))
+    }
+}
+
+#[derive(Debug, Clone)]
+pub struct RenderPassColorAttachment {
+    /// The actual color attachment.
+    pub attachment: TextureAttachment,
+
+    /// The resolve target for this color attachment, if any.
+    pub resolve_target: Option<TextureAttachment>,
+
+    /// What operations will be performed on this color attachment.
+    pub ops: Operations<Color>,
+}
+
+#[derive(Debug, Clone)]
+pub struct RenderPassDepthStencilAttachment {
+    pub attachment: TextureAttachment,
+    /// What operations will be performed on the depth part of the attachment.
+    pub depth_ops: Option<Operations<f32>>,
+    /// What operations will be performed on the stencil part of the attachment.
+    pub stencil_ops: Option<Operations<u32>>,
+}
+
+// A set of pipeline bindings and draw calls with color and depth outputs
+#[derive(Debug, Clone)]
+pub struct PassDescriptor {
+    pub color_attachments: Vec<RenderPassColorAttachment>,
+    pub depth_stencil_attachment: Option<RenderPassDepthStencilAttachment>,
+    pub sample_count: u32,
+}

pipelined/bevy_render2/src/render_graph/nodes/window_swap_chain_node.rs
@@ -1,0 +1,99 @@
+use crate::{
+    render_graph::{Node, ResourceSlotInfo, ResourceSlots},
+    render_resource::{RenderResourceId, RenderResourceType, SwapChainDescriptor},
+    renderer::RenderContext,
+};
+use bevy_ecs::world::World;
+use bevy_utils::HashMap;
+use bevy_window::WindowId;
+use raw_window_handle::{HasRawWindowHandle, RawWindowHandle};
+use std::borrow::Cow;
+use std::ops::{Deref, DerefMut};
+
+pub struct ExtractedWindow {
+    pub id: WindowId,
+    pub handle: RawWindowHandleWrapper,
+    pub physical_width: u32,
+    pub physical_height: u32,
+    pub vsync: bool,
+}
+
+#[derive(Default)]
+pub struct ExtractedWindows {
+    pub windows: HashMap<WindowId, ExtractedWindow>,
+}
+
+impl Deref for ExtractedWindows {
+    type Target = HashMap<WindowId, ExtractedWindow>;
+
+    fn deref(&self) -> &Self::Target {
+        &self.windows
+    }
+}
+
+impl DerefMut for ExtractedWindows {
+    fn deref_mut(&mut self) -> &mut Self::Target {
+        &mut self.windows
+    }
+}
+
+pub struct RawWindowHandleWrapper(pub RawWindowHandle);
+
+// TODO: THIS IS NOT SAFE ... ONLY FOR PROTOTYPING
+unsafe impl Send for RawWindowHandleWrapper {}
+unsafe impl Sync for RawWindowHandleWrapper {}
+
+// TODO: safe?
+unsafe impl HasRawWindowHandle for RawWindowHandleWrapper {
+    fn raw_window_handle(&self) -> RawWindowHandle {
+        self.0.clone()
+    }
+}
+
+pub struct WindowSwapChainNode {
+    window_id: WindowId,
+}
+
+impl WindowSwapChainNode {
+    pub const OUT_TEXTURE: &'static str = "texture";
+
+    pub fn new(window_id: WindowId) -> Self {
+        WindowSwapChainNode { window_id }
+    }
+}
+
+impl Node for WindowSwapChainNode {
+    fn output(&self) -> &[ResourceSlotInfo] {
+        static OUTPUT: &[ResourceSlotInfo] = &[ResourceSlotInfo {
+            name: Cow::Borrowed(WindowSwapChainNode::OUT_TEXTURE),
+            resource_type: RenderResourceType::Texture,
+        }];
+        OUTPUT
+    }
+
+    fn update(
+        &mut self,
+        world: &World,
+        render_context: &mut dyn RenderContext,
+        _input: &ResourceSlots,
+        output: &mut ResourceSlots,
+    ) {
+        let windows = world.get_resource::<ExtractedWindows>().unwrap();
+        let window = windows
+            .get(&self.window_id)
+            .expect("Window swapchain node refers to a non-existent window.");
+
+        let render_resource_context = render_context.resources_mut();
+        let swap_chain_descriptor = SwapChainDescriptor {
+            window_id: window.id,
+            format: crate::texture::TextureFormat::Bgra8UnormSrgb,
+            width: window.physical_width,
+            height: window.physical_height,
+            vsync: window.vsync,
+        };
+
+        let swap_chain_texture =
+            render_resource_context.next_swap_chain_texture(&swap_chain_descriptor);
+        output.set(0, RenderResourceId::Texture(swap_chain_texture));
+    }
+}

pipelined/bevy_render2/src/render_resource/buffer_vec.rs
@@ -1,0 +1,124 @@
+use crate::{
+    render_resource::{BufferId, BufferInfo, BufferMapMode, BufferUsage},
+    renderer::{RenderContext, RenderResources},
+};
+use bevy_core::{Pod, cast_slice};
+
+pub struct BufferVec<T: Pod> {
+    values: Vec<T>,
+    staging_buffer: Option<BufferId>,
+    buffer: Option<BufferId>,
+    capacity: usize,
+    item_size: usize,
+    buffer_usage: BufferUsage,
+}
+
+impl<T: Pod> Default for BufferVec<T> {
+    fn default() -> Self {
+        Self {
+            values: Vec::new(),
+            staging_buffer: None,
+            buffer: None,
+            capacity: 0,
+            buffer_usage: BufferUsage::all(),
+            item_size: std::mem::size_of::<T>(),
+        }
+    }
+}
+
+impl<T: Pod> BufferVec<T> {
+    pub fn new(buffer_usage: BufferUsage) -> Self {
+        Self {
+            buffer_usage,
+            ..Default::default()
+        }
+    }
+    #[inline]
+    pub fn staging_buffer(&self) -> Option<BufferId> {
+        self.staging_buffer
+    }
+
+    #[inline]
+    pub fn buffer(&self) -> Option<BufferId> {
+        self.buffer
+    }
+
+    #[inline]
+    pub fn capacity(&self) -> usize {
+        self.capacity
+    }
+
+    pub fn push(&mut self, value: T) -> usize {
+        if self.values.len() < self.capacity {
+            let index = self.values.len();
+            self.values.push(value);
+            index
+        } else {
+            panic!(
+                "Cannot push value because capacity of {} has been reached",
+                self.capacity
+            );
+        }
+    }
+
+    pub fn reserve(&mut self, capacity: usize, render_resources: &RenderResources) {
+        if capacity > self.capacity {
+            self.capacity = capacity;
+            if let Some(staging_buffer) = self.staging_buffer.take() {
+                render_resources.remove_buffer(staging_buffer);
+            }
+
+            if let Some(buffer) = self.buffer.take() {
+                render_resources.remove_buffer(buffer);
+            }
+
+            let size = self.item_size * capacity;
+            self.staging_buffer = Some(render_resources.create_buffer(BufferInfo {
+                size,
+                buffer_usage: BufferUsage::COPY_SRC | BufferUsage::MAP_WRITE,
+                mapped_at_creation: false,
+            }));
+            self.buffer = Some(render_resources.create_buffer(BufferInfo {
+                size,
+                buffer_usage: BufferUsage::COPY_DST | self.buffer_usage,
+                mapped_at_creation: false,
+            }));
+        }
+    }
+
+    pub fn reserve_and_clear(&mut self, capacity: usize, render_resources: &RenderResources) {
+        self.clear();
+        self.reserve(capacity, render_resources);
+    }
+
+    pub fn write_to_staging_buffer(&self, render_resources: &RenderResources) {
+        if let Some(staging_buffer) = self.staging_buffer {
+            let size = self.values.len() * self.item_size;
+            render_resources.map_buffer(staging_buffer, BufferMapMode::Write);
+            render_resources.write_mapped_buffer(
+                staging_buffer,
+                0..size as u64,
+                &mut |data, _renderer| {
+                    let bytes: &[u8] = cast_slice(&self.values);
+                    data.copy_from_slice(bytes);
+                },
+            );
+            render_resources.unmap_buffer(staging_buffer);
+        }
+    }
+    pub fn write_to_uniform_buffer(&self, render_context: &mut dyn RenderContext) {
+        if let (Some(staging_buffer), Some(uniform_buffer)) = (self.staging_buffer, self.buffer) {
+            render_context.copy_buffer_to_buffer(
+                staging_buffer,
+                0,
+                uniform_buffer,
+                0,
+                (self.values.len() * self.item_size) as u64,
+            );
+        }
+    }
+
+    pub fn clear(&mut self) {
+        self.values.clear();
+    }
+}