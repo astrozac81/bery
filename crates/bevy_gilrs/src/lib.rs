astrozac81/bery

commit ef14971576a6bee5ff9ff3669d9c4288d101cfb1

Resolve (most) internal system ambiguities (#1606)
* Adds labels and orderings to systems that need them (uses the new many-to-many labels for InputSystem)
* Removes the Event, PreEvent, Scene, and Ui stages in favor of First, PreUpdate, and PostUpdate (there is more collapsing potential, such as the Asset stages and _maybe_ removing First, but those have more nuance so they should be handled separately)
* Ambiguity detection now prints component conflicts
* Removed broken change filters from flex calculation (which implicitly relied on the z-update system always modifying translation.z). This will require more work to make it behave as expected so i just removed it (and it was already doing this work every frame).

crates/bevy_gilrs/src/lib.rs
@@ -24,7 +24,7 @@ impl Plugin for GilrsPlugin {
                         gilrs_event_startup_system.exclusive_system(),
                     )
                     .add_system_to_stage(
-                        CoreStage::PreEvent,
+                        CoreStage::PreUpdate,
                         gilrs_event_system.exclusive_system(),
                     );
             }

crates/bevy_app/src/lib.rs
@@ -31,10 +31,6 @@ pub enum CoreStage {
     Startup,
     /// Name of app stage that runs before all other app stages
     First,
-    /// Name of app stage that runs before EVENT
-    PreEvent,
-    /// Name of app stage that updates events. Runs before UPDATE
-    Event,
     /// Name of app stage responsible for performing setup before an update. Runs before UPDATE.
     PreUpdate,
     /// Name of app stage responsible for doing most app logic. Systems should be registered here by default.

crates/bevy_core/src/lib.rs
@@ -17,13 +17,23 @@ pub mod prelude {
 }
 
 use bevy_app::prelude::*;
-use bevy_ecs::{entity::Entity, system::IntoSystem};
+use bevy_ecs::{
+    entity::Entity,
+    schedule::{ExclusiveSystemDescriptorCoercion, SystemLabel},
+    system::{IntoExclusiveSystem, IntoSystem},
+};
 use bevy_utils::HashSet;
 use std::ops::Range;
 
 /// Adds core functionality to Apps.
 #[derive(Default)]
 pub struct CorePlugin;
+
+#[derive(Debug, PartialEq, Eq, Clone, Hash, SystemLabel)]
+pub enum CoreSystem {
+    /// Updates the elapsed time. Any system that interacts with [Time] component should run after this.
+    Time,
+}
 
 impl Plugin for CorePlugin {
     fn build(&self, app: &mut AppBuilder) {
@@ -44,7 +54,11 @@ impl Plugin for CorePlugin {
             .register_type::<Labels>()
             .register_type::<Range<f32>>()
             .register_type::<Timer>()
-            .add_system_to_stage(CoreStage::First, time_system.system())
+            // time system is added as an "exclusive system" to ensure it runs before other systems in CoreStage::First
+            .add_system_to_stage(
+                CoreStage::First,
+                time_system.exclusive_system().label(CoreSystem::Time),
+            )
             .add_startup_system_to_stage(StartupStage::PostStartup, entity_labels_system.system())
             .add_system_to_stage(CoreStage::PostUpdate, entity_labels_system.system());
 

crates/bevy_input/src/lib.rs
@@ -7,7 +7,10 @@ pub mod system;
 pub mod touch;
 
 pub use axis::*;
-use bevy_ecs::system::IntoSystem;
+use bevy_ecs::{
+    schedule::{ParallelSystemDescriptorCoercion, SystemLabel},
+    system::IntoSystem,
+};
 pub use input::*;
 
 pub mod prelude {
@@ -37,27 +40,46 @@ use gamepad::{
 #[derive(Default)]
 pub struct InputPlugin;
 
+#[derive(Debug, PartialEq, Eq, Clone, Hash, SystemLabel)]
+pub struct InputSystem;
+
 impl Plugin for InputPlugin {
     fn build(&self, app: &mut AppBuilder) {
-        app.add_event::<KeyboardInput>()
+        app
+            // keyboard
+            .add_event::<KeyboardInput>()
+            .init_resource::<Input<KeyCode>>()
+            .add_system_to_stage(
+                CoreStage::PreUpdate,
+                keyboard_input_system.system().label(InputSystem),
+            )
+            // mouse
             .add_event::<MouseButtonInput>()
             .add_event::<MouseMotion>()
             .add_event::<MouseWheel>()
-            .init_resource::<Input<KeyCode>>()
-            .add_system_to_stage(CoreStage::Event, keyboard_input_system.system())
             .init_resource::<Input<MouseButton>>()
-            .add_system_to_stage(CoreStage::Event, mouse_button_input_system.system())
+            .add_system_to_stage(
+                CoreStage::PreUpdate,
+                mouse_button_input_system.system().label(InputSystem),
+            )
+            // gamepad
             .add_event::<GamepadEvent>()
             .add_event::<GamepadEventRaw>()
             .init_resource::<GamepadSettings>()
             .init_resource::<Input<GamepadButton>>()
             .init_resource::<Axis<GamepadAxis>>()
             .init_resource::<Axis<GamepadButton>>()
-            .add_system_to_stage(CoreStage::Event, gamepad_event_system.system())
-            .add_startup_system_to_stage(StartupStage::Startup, gamepad_event_system.system())
+            .add_system_to_stage(
+                CoreStage::PreUpdate,
+                gamepad_event_system.system().label(InputSystem),
+            )
+            // touch
             .add_event::<TouchInput>()
             .init_resource::<Touches>()
-            .add_system_to_stage(CoreStage::Event, touch_screen_input_system.system());
+            .add_system_to_stage(
+                CoreStage::PreUpdate,
+                touch_screen_input_system.system().label(InputSystem),
+            );
     }
 }
 

crates/bevy_render/src/lib.rs
@@ -13,9 +13,10 @@ pub mod texture;
 pub mod wireframe;
 
 use bevy_ecs::{
-    schedule::SystemStage,
+    schedule::{ParallelSystemDescriptorCoercion, SystemStage},
     system::{IntoExclusiveSystem, IntoSystem},
 };
+use bevy_transform::TransformSystem;
 use draw::Visible;
 pub use once_cell;
 
@@ -37,7 +38,7 @@ use crate::prelude::*;
 use base::Msaa;
 use bevy_app::prelude::*;
 use bevy_asset::{AddAsset, AssetStage};
-use bevy_ecs::schedule::StageLabel;
+use bevy_ecs::schedule::{StageLabel, SystemLabel};
 use camera::{
     ActiveCameras, Camera, DepthCalculation, OrthographicProjection, PerspectiveProjection,
     RenderLayers, ScalingMode, VisibleEntities, WindowOrigin,
@@ -56,6 +57,11 @@ use shader::ShaderLoader;
 use texture::HdrTextureLoader;
 #[cfg(feature = "png")]
 use texture::ImageTextureLoader;
+
+#[derive(Debug, Hash, PartialEq, Eq, Clone, SystemLabel)]
+pub enum RenderSystem {
+    VisibleEntities,
+}
 
 /// The names of "render" App stages
 #[derive(Debug, Hash, PartialEq, Eq, Clone, StageLabel)]
@@ -157,16 +163,22 @@ impl Plugin for RenderPlugin {
         )
         .add_system_to_stage(
             CoreStage::PostUpdate,
-            camera::camera_system::<OrthographicProjection>.system(),
+            camera::camera_system::<OrthographicProjection>
+                .system()
+                .before(RenderSystem::VisibleEntities),
         )
         .add_system_to_stage(
             CoreStage::PostUpdate,
-            camera::camera_system::<PerspectiveProjection>.system(),
+            camera::camera_system::<PerspectiveProjection>
+                .system()
+                .before(RenderSystem::VisibleEntities),
         )
-        // registration order matters here. this must come after all camera_system::<T> systems
         .add_system_to_stage(
             CoreStage::PostUpdate,
-            camera::visible_entities_system.system(),
+            camera::visible_entities_system
+                .system()
+                .label(RenderSystem::VisibleEntities)
+                .after(TransformSystem::TransformPropagate),
         )
         .add_system_to_stage(
             RenderStage::RenderResource,

crates/bevy_scene/src/lib.rs
@@ -19,18 +19,10 @@ pub mod prelude {
 
 use bevy_app::prelude::*;
 use bevy_asset::AddAsset;
-use bevy_ecs::{
-    schedule::{StageLabel, SystemStage},
-    system::IntoExclusiveSystem,
-};
+use bevy_ecs::{schedule::ExclusiveSystemDescriptorCoercion, system::IntoExclusiveSystem};
 
 #[derive(Default)]
 pub struct ScenePlugin;
-
-#[derive(Debug, Hash, PartialEq, Eq, Clone, StageLabel)]
-enum SceneStage {
-    SceneStage,
-}
 
 impl Plugin for ScenePlugin {
     fn build(&self, app: &mut AppBuilder) {
@@ -38,14 +30,9 @@ impl Plugin for ScenePlugin {
             .add_asset::<Scene>()
             .init_asset_loader::<SceneLoader>()
             .init_resource::<SceneSpawner>()
-            .add_stage_after(
-                CoreStage::Event,
-                SceneStage::SceneStage,
-                SystemStage::parallel(),
-            )
             .add_system_to_stage(
-                SceneStage::SceneStage,
-                scene_spawner_system.exclusive_system(),
+                CoreStage::PreUpdate,
+                scene_spawner_system.exclusive_system().at_end(),
             );
     }
 }

crates/bevy_ui/src/lib.rs
@@ -1,16 +1,15 @@
 mod anchors;
-pub mod entity;
 mod flex;
 mod focus;
 mod margins;
 mod render;
 mod ui_node;
+
+pub mod entity;
 pub mod update;
 pub mod widget;
 
 pub use anchors::*;
-use bevy_math::{Rect, Size};
-use bevy_render::RenderStage;
 pub use flex::*;
 pub use focus::*;
 pub use margins::*;
@@ -23,26 +22,22 @@ pub mod prelude {
 
 use bevy_app::prelude::*;
 use bevy_ecs::{
-    schedule::{ParallelSystemDescriptorCoercion, StageLabel, SystemLabel, SystemStage},
+    schedule::{ParallelSystemDescriptorCoercion, SystemLabel},
     system::IntoSystem,
 };
+use bevy_input::InputSystem;
+use bevy_math::{Rect, Size};
+use bevy_render::RenderStage;
+use bevy_transform::TransformSystem;
 use update::ui_z_system;
 
 #[derive(Default)]
 pub struct UiPlugin;
 
-#[derive(Debug, Hash, PartialEq, Eq, Clone, StageLabel)]
-pub enum UiStage {
-    Ui,
-}
-
 #[derive(Debug, Hash, PartialEq, Eq, Clone, SystemLabel)]
 pub enum UiSystem {
+    /// After this label, the ui flex state has been updated
     Flex,
-}
-
-pub mod system {
-    pub const FLEX: &str = "flex";
 }
 
 impl Plugin for UiPlugin {
@@ -63,19 +58,33 @@ impl Plugin for UiPlugin {
             .register_type::<Rect<Val>>()
             .register_type::<Style>()
             .register_type::<Val>()
-            .add_stage_before(CoreStage::PostUpdate, UiStage::Ui, SystemStage::parallel())
-            .add_system_to_stage(CoreStage::PreUpdate, ui_focus_system.system())
+            .add_system_to_stage(
+                CoreStage::PreUpdate,
+                ui_focus_system.system().after(InputSystem),
+            )
             // add these stages to front because these must run before transform update systems
             .add_system_to_stage(
-                UiStage::Ui,
+                CoreStage::PostUpdate,
                 widget::text_system.system().before(UiSystem::Flex),
             )
             .add_system_to_stage(
-                UiStage::Ui,
+                CoreStage::PostUpdate,
                 widget::image_node_system.system().before(UiSystem::Flex),
             )
-            .add_system_to_stage(UiStage::Ui, flex_node_system.system().label(UiSystem::Flex))
-            .add_system_to_stage(UiStage::Ui, ui_z_system.system())
+            .add_system_to_stage(
+                CoreStage::PostUpdate,
+                flex_node_system
+                    .system()
+                    .label(UiSystem::Flex)
+                    .before(TransformSystem::TransformPropagate),
+            )
+            .add_system_to_stage(
+                CoreStage::PostUpdate,
+                ui_z_system
+                    .system()
+                    .after(UiSystem::Flex)
+                    .before(TransformSystem::TransformPropagate),
+            )
             .add_system_to_stage(RenderStage::Draw, widget::draw_text_system.system());
 
         crate::render::add_ui_graph(app.world_mut());

crates/bevy_winit/src/lib.rs
@@ -10,7 +10,7 @@ use bevy_input::{
 pub use winit_config::*;
 pub use winit_windows::*;
 
-use bevy_app::{App, AppBuilder, AppExit, Events, ManualEventReader, Plugin};
+use bevy_app::{App, AppBuilder, AppExit, CoreStage, Events, ManualEventReader, Plugin};
 use bevy_ecs::{system::IntoExclusiveSystem, world::World};
 use bevy_math::{ivec2, Vec2};
 use bevy_utils::tracing::{error, trace, warn};
@@ -42,7 +42,7 @@ impl Plugin for WinitPlugin {
     fn build(&self, app: &mut AppBuilder) {
         app.init_resource::<WinitWindows>()
             .set_runner(winit_runner)
-            .add_system(change_window.exclusive_system());
+            .add_system_to_stage(CoreStage::PostUpdate, change_window.exclusive_system());
     }
 }
 

commit 4c4595ff55a413be500fbaccc34addbc44374869

Schedule gilrs system before input systems (#2989)
# Objective

Previously, the gilrs system had no explicit relationship to the input
systems. This could potentially cause it to be scheduled after the
input systems, leading to a one-frame lag in gamepad inputs.

This was a regression introduced in #1606 which removed the `PreEvent` stage

## Solution

This adds an explicit `before` relationship to the gilrs plugin,
ensuring that raw gamepad events will be processed on the same frame
that they are generated.

crates/bevy_gilrs/src/lib.rs
@@ -2,6 +2,8 @@ mod converter;
 mod gilrs_system;
 
 use bevy_app::{App, CoreStage, Plugin, StartupStage};
+use bevy_ecs::schedule::ParallelSystemDescriptorCoercion;
+use bevy_input::InputSystem;
 use bevy_utils::tracing::error;
 use gilrs::GilrsBuilder;
 use gilrs_system::{gilrs_event_startup_system, gilrs_event_system};
@@ -22,7 +24,10 @@ impl Plugin for GilrsPlugin {
                         StartupStage::PreStartup,
                         gilrs_event_startup_system,
                     )
-                    .add_system_to_stage(CoreStage::PreUpdate, gilrs_event_system);
+                    .add_system_to_stage(
+                        CoreStage::PreUpdate,
+                        gilrs_event_system.before(InputSystem),
+                    );
             }
             Err(err) => error!("Failed to start Gilrs. {}", err),
         }