bevyengine/bevy

commit 83d6600267f54faa9ead083516c9f42c16491ed5

Implement minimal reflection probes (fixed macOS, iOS, and Android). (#11366)
This pull request re-submits #10057, which was backed out for breaking
macOS, iOS, and Android. I've tested this version on macOS and Android
and on the iOS simulator.

# Objective

This pull request implements *reflection probes*, which generalize
environment maps to allow for multiple environment maps in the same
scene, each of which has an axis-aligned bounding box. This is a
standard feature of physically-based renderers and was inspired by [the
corresponding feature in Blender's Eevee renderer].

## Solution

This is a minimal implementation of reflection probes that allows
artists to define cuboid bounding regions associated with environment
maps. For every view, on every frame, a system builds up a list of the
nearest 4 reflection probes that are within the view's frustum and
supplies that list to the shader. The PBR fragment shader searches
through the list, finds the first containing reflection probe, and uses
it for indirect lighting, falling back to the view's environment map if
none is found. Both forward and deferred renderers are fully supported.

A reflection probe is an entity with a pair of components, *LightProbe*
and *EnvironmentMapLight* (as well as the standard *SpatialBundle*, to
position it in the world). The *LightProbe* component (along with the
*Transform*) defines the bounding region, while the
*EnvironmentMapLight* component specifies the associated diffuse and
specular cubemaps.

A frequent question is "why two components instead of just one?" The
advantages of this setup are:

1. It's readily extensible to other types of light probes, in particular
*irradiance volumes* (also known as ambient cubes or voxel global
illumination), which use the same approach of bounding cuboids. With a
single component that applies to both reflection probes and irradiance
volumes, we can share the logic that implements falloff and blending

crates/bevy_pbr/src/light_probe/environment_map.wgsl
@@ -1,0 +1,176 @@
+#define_import_path bevy_pbr::environment_map
+
+#import bevy_pbr::mesh_view_bindings as bindings
+#import bevy_pbr::mesh_view_bindings::light_probes
+
+struct EnvironmentMapLight {
+    diffuse: vec3<f32>,
+    specular: vec3<f32>,
+};
+
+struct EnvironmentMapRadiances {
+    irradiance: vec3<f32>,
+    radiance: vec3<f32>,
+}
+
+// Define two versions of this function, one for the case in which there are
+// multiple light probes and one for the case in which only the view light probe
+// is present.
+
+#ifdef MULTIPLE_LIGHT_PROBES_IN_ARRAY
+
+fn compute_radiances(
+    perceptual_roughness: f32,
+    N: vec3<f32>,
+    R: vec3<f32>,
+    world_position: vec3<f32>,
+) -> EnvironmentMapRadiances {
+    var radiances: EnvironmentMapRadiances;
+
+    // Search for a reflection probe that contains the fragment.
+    //
+    // TODO: Interpolate between multiple reflection probes.
+    var cubemap_index: i32 = -1;
+    var intensity: f32 = 1.0;
+    for (var reflection_probe_index: i32 = 0;
+            reflection_probe_index < light_probes.reflection_probe_count;
+            reflection_probe_index += 1) {
+        let reflection_probe = light_probes.reflection_probes[reflection_probe_index];
+
+        // Unpack the inverse transform.
+        let inverse_transpose_transform = mat4x4<f32>(
+            reflection_probe.inverse_transpose_transform[0],
+            reflection_probe.inverse_transpose_transform[1],
+            reflection_probe.inverse_transpose_transform[2],
+            vec4<f32>(0.0, 0.0, 0.0, 1.0));
+        let inverse_transform = transpose(inverse_transpose_transform);
+
+        // Check to see if the transformed point is inside the unit cube
+        // centered at the origin.
+        let probe_space_pos = (inverse_transform * vec4<f32>(world_position, 1.0)).xyz;
+        if (all(abs(probe_space_pos) <= vec3(0.5))) {
+            cubemap_index = reflection_probe.cubemap_index;
+            intensity = reflection_probe.intensity;
+            break;
+        }
+    }
+
+    // If we didn't find a reflection probe, use the view environment map if applicable.
+    if (cubemap_index < 0) {
+        cubemap_index = light_probes.view_cubemap_index;
+        intensity = light_probes.intensity_for_view;
+    }
+
+    // If there's no cubemap, bail out.
+    if (cubemap_index < 0) {
+        radiances.irradiance = vec3(0.0);
+        radiances.radiance = vec3(0.0);
+        return radiances;
+    }
+
+    // Split-sum approximation for image based lighting: https://cdn2.unrealengine.com/Resources/files/2013SiggraphPresentationsNotes-26915738.pdf
+    let radiance_level = perceptual_roughness * f32(textureNumLevels(bindings::specular_environment_maps[cubemap_index]) - 1u);
+
+#ifndef LIGHTMAP
+    radiances.irradiance = textureSampleLevel(
+        bindings::diffuse_environment_maps[cubemap_index],
+        bindings::environment_map_sampler,
+        vec3(N.xy, -N.z),
+        0.0).rgb * intensity;
+#endif  // LIGHTMAP
+
+    radiances.radiance = textureSampleLevel(
+        bindings::specular_environment_maps[cubemap_index],
+        bindings::environment_map_sampler,
+        vec3(R.xy, -R.z),
+        radiance_level).rgb * intensity;
+
+    return radiances;
+}
+
+#else   // MULTIPLE_LIGHT_PROBES_IN_ARRAY
+
+fn compute_radiances(
+    perceptual_roughness: f32,
+    N: vec3<f32>,
+    R: vec3<f32>,
+    world_position: vec3<f32>,
+) -> EnvironmentMapRadiances {
+    var radiances: EnvironmentMapRadiances;
+
+    if (light_probes.view_cubemap_index < 0) {
+        radiances.irradiance = vec3(0.0);
+        radiances.radiance = vec3(0.0);
+        return radiances;
+    }
+
+    // Split-sum approximation for image based lighting: https://cdn2.unrealengine.com/Resources/files/2013SiggraphPresentationsNotes-26915738.pdf
+    // Technically we could use textureNumLevels(specular_environment_map) - 1 here, but we use a uniform
+    // because textureNumLevels() does not work on WebGL2
+    let radiance_level = perceptual_roughness * f32(light_probes.smallest_specular_mip_level_for_view);
+
+    let intensity = light_probes.intensity_for_view;
+
+#ifndef LIGHTMAP
+    radiances.irradiance = textureSampleLevel(
+        bindings::diffuse_environment_map,
+        bindings::environment_map_sampler,
+        vec3(N.xy, -N.z),
+        0.0).rgb * intensity;
+#endif  // LIGHTMAP
+
+    radiances.radiance = textureSampleLevel(
+        bindings::specular_environment_map,
+        bindings::environment_map_sampler,
+        vec3(R.xy, -R.z),
+        radiance_level).rgb * intensity;
+
+    return radiances;
+}
+
+#endif  // MULTIPLE_LIGHT_PROBES_IN_ARRAY
+
+fn environment_map_light(
+    perceptual_roughness: f32,
+    roughness: f32,
+    diffuse_color: vec3<f32>,
+    NdotV: f32,
+    f_ab: vec2<f32>,
+    N: vec3<f32>,
+    R: vec3<f32>,
+    F0: vec3<f32>,
+    world_position: vec3<f32>,
+) -> EnvironmentMapLight {
+    let radiances = compute_radiances(perceptual_roughness, N, R, world_position);
+
+    // No real world material has specular values under 0.02, so we use this range as a
+    // "pre-baked specular occlusion" that extinguishes the fresnel term, for artistic control.
+    // See: https://google.github.io/filament/Filament.html#specularocclusion
+    let specular_occlusion = saturate(dot(F0, vec3(50.0 * 0.33)));
+
+    // Multiscattering approximation: https://www.jcgt.org/published/0008/01/03/paper.pdf
+    // Useful reference: https://bruop.github.io/ibl
+    let Fr = max(vec3(1.0 - roughness), F0) - F0;
+    let kS = F0 + Fr * pow(1.0 - NdotV, 5.0);
+    let Ess = f_ab.x + f_ab.y;
+    let FssEss = kS * Ess * specular_occlusion;
+    let Ems = 1.0 - Ess;
+    let Favg = F0 + (1.0 - F0) / 21.0;
+    let Fms = FssEss * Favg / (1.0 - Ems * Favg);
+    let FmsEms = Fms * Ems;
+    let Edss = 1.0 - (FssEss + FmsEms);
+    let kD = diffuse_color * Edss;
+
+    var out: EnvironmentMapLight;
+
+    // If there's a lightmap, ignore the diffuse component of the reflection
+    // probe, so we don't double-count light.
+#ifdef LIGHTMAP
+    out.diffuse = vec3(0.0);
+#else
+    out.diffuse = (FmsEms + kD) * radiances.irradiance;
+#endif
+
+    out.specular = FssEss * radiances.radiance;
+    return out;
+}

crates/bevy_pbr/src/environment_map/environment_map.wgsl
@@ -1,50 +1,0 @@
-#define_import_path bevy_pbr::environment_map
-
-#import bevy_pbr::mesh_view_bindings as bindings;
-
-struct EnvironmentMapLight {
-    diffuse: vec3<f32>,
-    specular: vec3<f32>,
-};
-
-fn environment_map_light(
-    perceptual_roughness: f32,
-    roughness: f32,
-    diffuse_color: vec3<f32>,
-    NdotV: f32,
-    f_ab: vec2<f32>,
-    N: vec3<f32>,
-    R: vec3<f32>,
-    F0: vec3<f32>,
-) -> EnvironmentMapLight {
-
-    // Split-sum approximation for image based lighting: https://cdn2.unrealengine.com/Resources/files/2013SiggraphPresentationsNotes-26915738.pdf
-    // Technically we could use textureNumLevels(environment_map_specular) - 1 here, but we use a uniform
-    // because textureNumLevels() does not work on WebGL2
-    let radiance_level = perceptual_roughness * f32(bindings::lights.environment_map_smallest_specular_mip_level);
-    let irradiance = textureSampleLevel(bindings::environment_map_diffuse, bindings::environment_map_sampler, vec3(N.xy, -N.z), 0.0).rgb;
-    let radiance = textureSampleLevel(bindings::environment_map_specular, bindings::environment_map_sampler, vec3(R.xy, -R.z), radiance_level).rgb;
-
-    // No real world material has specular values under 0.02, so we use this range as a
-    // "pre-baked specular occlusion" that extinguishes the fresnel term, for artistic control.
-    // See: https://google.github.io/filament/Filament.html#specularocclusion
-    let specular_occlusion = saturate(dot(F0, vec3(50.0 * 0.33)));
-
-    // Multiscattering approximation: https://www.jcgt.org/published/0008/01/03/paper.pdf
-    // Useful reference: https://bruop.github.io/ibl
-    let Fr = max(vec3(1.0 - roughness), F0) - F0;
-    let kS = F0 + Fr * pow(1.0 - NdotV, 5.0);
-    let Ess = f_ab.x + f_ab.y;
-    let FssEss = kS * Ess * specular_occlusion;
-    let Ems = 1.0 - Ess;
-    let Favg = F0 + (1.0 - F0) / 21.0;
-    let Fms = FssEss * Favg / (1.0 - Ems * Favg);
-    let FmsEms = Fms * Ems;
-    let Edss = 1.0 - (FssEss + FmsEms);
-    let kD = diffuse_color * Edss;
-
-    var out: EnvironmentMapLight;
-    out.diffuse = (FmsEms + kD) * irradiance * bindings::lights.environment_map_intensity;
-    out.specular = FssEss * radiance * bindings::lights.environment_map_intensity;
-    return out;
-}

Cargo.toml
@@ -2508,6 +2508,17 @@ name = "fallback_image"
 path = "examples/shader/fallback_image.rs"
 doc-scrape-examples = true
 
+[[example]]
+name = "reflection_probes"
+path = "examples/3d/reflection_probes.rs"
+doc-scrape-examples = true
+
+[package.metadata.example.reflection_probes]
+name = "Reflection Probes"
+description = "Demonstrates reflection probes"
+category = "3D Rendering"
+wasm = false
+
 [package.metadata.example.fallback_image]
 hidden = true
 

crates/bevy_core_pipeline/src/tonemapping/tonemapping_shared.wgsl
@@ -7,8 +7,8 @@
     @group(0) @binding(3) var dt_lut_texture: texture_3d<f32>;
     @group(0) @binding(4) var dt_lut_sampler: sampler;
 #else
-    @group(0) @binding(15) var dt_lut_texture: texture_3d<f32>;
-    @group(0) @binding(16) var dt_lut_sampler: sampler;
+    @group(0) @binding(16) var dt_lut_texture: texture_3d<f32>;
+    @group(0) @binding(17) var dt_lut_sampler: sampler;
 #endif
 
 fn sample_current_lut(p: vec3<f32>) -> vec3<f32> {

crates/bevy_internal/Cargo.toml
@@ -58,7 +58,10 @@ symphonia-vorbis = ["bevy_audio/symphonia-vorbis"]
 symphonia-wav = ["bevy_audio/symphonia-wav"]
 
 # Shader formats
-shader_format_glsl = ["bevy_render/shader_format_glsl"]
+shader_format_glsl = [
+  "bevy_render/shader_format_glsl",
+  "bevy_pbr?/shader_format_glsl",
+]
 shader_format_spirv = ["bevy_render/shader_format_spirv"]
 
 serialize = [

crates/bevy_pbr/Cargo.toml
@@ -10,6 +10,7 @@ keywords = ["bevy"]
 
 [features]
 webgl = []
+shader_format_glsl = ["naga_oil/glsl"]
 pbr_transmission_textures = []
 
 [dependencies]
@@ -34,8 +35,17 @@ fixedbitset = "0.4"
 # direct dependency required for derive macro
 bytemuck = { version = "1", features = ["derive"] }
 radsort = "0.1"
-naga_oil = "0.11"
+smallvec = "1.6"
 thread_local = "1.0"
+
+[target.'cfg(target_arch = "wasm32")'.dependencies]
+naga_oil = { version = "0.11" }
+
+[target.'cfg(not(target_arch = "wasm32"))'.dependencies]
+# Omit the `glsl` feature in non-WebAssembly by default.
+naga_oil = { version = "0.11", default-features = false, features = [
+  "test_shader",
+] }
 
 [lints]
 workspace = true

crates/bevy_pbr/src/deferred/mod.rs
@@ -1,4 +1,7 @@
-use crate::{MeshPipeline, MeshViewBindGroup, ScreenSpaceAmbientOcclusionSettings};
+use crate::{
+    environment_map::RenderViewEnvironmentMaps, MeshPipeline, MeshViewBindGroup,
+    ScreenSpaceAmbientOcclusionSettings, ViewLightProbesUniformOffset,
+};
 use bevy_app::prelude::*;
 use bevy_asset::{load_internal_asset, Handle};
 use bevy_core_pipeline::{
@@ -14,25 +17,17 @@ use bevy_render::{
     extract_component::{
         ComponentUniforms, ExtractComponent, ExtractComponentPlugin, UniformComponentPlugin,
     },
-    render_asset::RenderAssets,
-    render_graph::{NodeRunError, RenderGraphContext, ViewNode, ViewNodeRunner},
-    render_resource::{
-        binding_types::uniform_buffer, Operations, PipelineCache, RenderPassDescriptor,
-    },
+    render_graph::{NodeRunError, RenderGraphApp, RenderGraphContext, ViewNode, ViewNodeRunner},
+    render_resource::binding_types::uniform_buffer,
+    render_resource::*,
     renderer::{RenderContext, RenderDevice},
-    texture::Image,
-    view::{ViewTarget, ViewUniformOffset},
-    Render, RenderSet,
-};
-
-use bevy_render::{
-    render_graph::RenderGraphApp, render_resource::*, texture::BevyDefault, view::ExtractedView,
-    RenderApp,
+    texture::BevyDefault,
+    view::{ExtractedView, ViewTarget, ViewUniformOffset},
+    Render, RenderApp, RenderSet,
 };
 
 use crate::{
-    EnvironmentMapLight, MeshPipelineKey, ShadowFilteringMethod, ViewFogUniformOffset,
-    ViewLightsUniformOffset,
+    MeshPipelineKey, ShadowFilteringMethod, ViewFogUniformOffset, ViewLightsUniformOffset,
 };
 
 pub struct DeferredPbrLightingPlugin;
@@ -151,6 +146,7 @@ impl ViewNode for DeferredOpaquePass3dPbrLightingNode {
         &'static ViewUniformOffset,
         &'static ViewLightsUniformOffset,
         &'static ViewFogUniformOffset,
+        &'static ViewLightProbesUniformOffset,
         &'static MeshViewBindGroup,
         &'static ViewTarget,
         &'static DeferredLightingIdDepthTexture,
@@ -165,6 +161,7 @@ impl ViewNode for DeferredOpaquePass3dPbrLightingNode {
             view_uniform_offset,
             view_lights_offset,
             view_fog_offset,
+            view_light_probes_offset,
             mesh_view_bind_group,
             target,
             deferred_lighting_id_depth_texture,
@@ -218,6 +215,7 @@ impl ViewNode for DeferredOpaquePass3dPbrLightingNode {
                 view_uniform_offset.offset,
                 view_lights_offset.offset,
                 view_fog_offset.offset,
+                **view_light_probes_offset,
             ],
         );
         render_pass.set_bind_group(1, &bind_group_1, &[]);
@@ -403,7 +401,6 @@ pub fn prepare_deferred_lighting_pipelines(
             &ExtractedView,
             Option<&Tonemapping>,
             Option<&DebandDither>,
-            Option<&EnvironmentMapLight>,
             Option<&ShadowFilteringMethod>,
             Has<ScreenSpaceAmbientOcclusionSettings>,
             (
@@ -411,20 +408,20 @@ pub fn prepare_deferred_lighting_pipelines(
                 Has<DepthPrepass>,
                 Has<MotionVectorPrepass>,
             ),
+            Has<RenderViewEnvironmentMaps>,
         ),
         With<DeferredPrepass>,
     >,
-    images: Res<RenderAssets<Image>>,
 ) {
     for (
         entity,
         view,
         tonemapping,
         dither,
-        environment_map,
         shadow_filter_method,
         ssao,
         (normal_prepass, depth_prepass, motion_vector_prepass),
+        has_environment_maps,
     ) in &views
     {
         let mut view_key = MeshPipelineKey::from_hdr(view.hdr);
@@ -471,11 +468,10 @@ pub fn prepare_deferred_lighting_pipelines(
             view_key |= MeshPipelineKey::SCREEN_SPACE_AMBIENT_OCCLUSION;
         }
 
-        let environment_map_loaded = match environment_map {
-            Some(environment_map) => environment_map.is_loaded(&images),
-            None => false,
-        };
-        if environment_map_loaded {
+        // We don't need to check to see whether the environment map is loaded
+        // because [`gather_light_probes`] already checked that for us before
+        // adding the [`RenderViewEnvironmentMaps`] component.
+        if has_environment_maps {
             view_key |= MeshPipelineKey::ENVIRONMENT_MAP;
         }
 

astrozac81/bery

commit 1eacb7c1aad4beb7daaa81ce362bd9c4820779d2

Workaround for ICE in the DXC shader compiler in debug builds with an `EnvironmentMapLight` (#11487)
# Objective

DXC+DX12 debug builds with an environment map have been broken since
https://github.com/bevyengine/bevy/pull/11366 merged due to an internal
compiler error in DXC. I tracked it down to a single `break` statement
and reported it upstream
(https://github.com/microsoft/DirectXShaderCompiler/issues/6183)

## Solution

Workaround the ICE by setting the for loop index variable to the max
value of the loop to avoid the `break` that's causing the ICE.

This works because it's the last thing in the for loop.

The `reflection_probes` and `pbr` examples both appear to still work
correctly.

crates/bevy_pbr/src/light_probe/environment_map.wgsl
@@ -51,7 +51,12 @@ fn compute_radiances(
         if (all(abs(probe_space_pos) <= vec3(0.5))) {
             cubemap_index = reflection_probe.cubemap_index;
             intensity = reflection_probe.intensity;
-            break;
+            // TODO: Workaround for ICE in DXC https://github.com/microsoft/DirectXShaderCompiler/issues/6183
+            // This works because it's the last thing that happens in the for loop, and will break as soon as it
+            // goes back to the top of the loop.
+            // We can't use `break` here because of the ICE.
+            // break;
+            reflection_probe_index = light_probes.reflection_probe_count;
         }
     }
 