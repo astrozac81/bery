astrozac81/bery

commit 36bf6e1cd9677d69daae8f24f9c5aafd354a21dd

style: simplify string formatting for readability (#15033)
# Objective

The goal of this change is to improve code readability and
maintainability.

examples/3d/irradiance_volumes.rs
@@ -351,12 +351,11 @@ impl AppStatus {
 
         Text::from_section(
             format!(
-                "{}\n{}\n{}\n{}\n{}",
-                CLICK_TO_MOVE_HELP_TEXT,
-                voxels_help_text,
-                irradiance_volume_help_text,
-                rotation_help_text,
-                switch_mesh_help_text
+                "{CLICK_TO_MOVE_HELP_TEXT}
+        {voxels_help_text}
+        {irradiance_volume_help_text}
+        {rotation_help_text}
+        {switch_mesh_help_text}"
             ),
             TextStyle::default(),
         )

examples/3d/anti_aliasing.rs
@@ -351,7 +351,7 @@ fn setup(
 /// Writes a simple menu item that can be on or off.
 fn draw_selectable_menu_item(ui: &mut String, label: &str, shortcut: char, enabled: bool) {
     let star = if enabled { "*" } else { "" };
-    let _ = writeln!(*ui, "({}) {}{}{}", shortcut, star, label, star);
+    let _ = writeln!(*ui, "({shortcut}) {star}{label}{star}");
 }
 
 /// Creates a colorful test pattern

examples/3d/color_grading.rs
@@ -459,9 +459,9 @@ impl Display for SelectedSectionColorGradingOption {
 impl Display for SelectedColorGradingOption {
     fn fmt(&self, f: &mut Formatter<'_>) -> fmt::Result {
         match self {
-            SelectedColorGradingOption::Global(option) => write!(f, "\"{}\"", option),
+            SelectedColorGradingOption::Global(option) => write!(f, "\"{option}\""),
             SelectedColorGradingOption::Section(section, option) => {
-                write!(f, "\"{}\" for \"{}\"", option, section)
+                write!(f, "\"{option}\" for \"{section}\"")
             }
         }
     }
@@ -633,7 +633,7 @@ fn update_ui_state(
 
 /// Creates the help text at the top left of the window.
 fn create_help_text(currently_selected_option: &SelectedColorGradingOption) -> String {
-    format!("Press Left/Right to adjust {}", currently_selected_option)
+    format!("Press Left/Right to adjust {currently_selected_option}")
 }
 
 /// Processes keyboard input to change the value of the currently-selected color

examples/app/headless_renderer.rs
@@ -533,7 +533,7 @@ fn update(
                     // Finally saving image to file, this heavy blocking operation is kept here
                     // for example simplicity, but in real app you should move it to a separate task
                     if let Err(e) = img.save(image_path) {
-                        panic!("Failed to save image: {}", e);
+                        panic!("Failed to save image: {e}");
                     };
                 }
                 if scene_controller.single_image {

examples/ecs/component_hooks.rs
@@ -69,10 +69,7 @@ fn setup(world: &mut World) {
         .on_add(|mut world, entity, component_id| {
             // You can access component data from within the hook
             let value = world.get::<MyComponent>(entity).unwrap().0;
-            println!(
-                "Component: {:?} added to: {:?} with value {:?}",
-                component_id, entity, value
-            );
+            println!("Component: {component_id:?} added to: {entity:?} with value {value:?}");
             // Or access resources
             world
                 .resource_mut::<MyComponentIndex>()
@@ -96,10 +93,7 @@ fn setup(world: &mut World) {
         // since it runs before the component is removed you can still access the component data
         .on_remove(|mut world, entity, component_id| {
             let value = world.get::<MyComponent>(entity).unwrap().0;
-            println!(
-                "Component: {:?} removed from: {:?} with value {:?}",
-                component_id, entity, value
-            );
+            println!("Component: {component_id:?} removed from: {entity:?} with value {value:?}");
             // You can also issue commands through `.commands()`
             world.commands().entity(entity).despawn();
         });

examples/ecs/dynamic.rs
@@ -50,7 +50,7 @@ fn main() {
     let mut component_names = HashMap::<String, ComponentId>::new();
     let mut component_info = HashMap::<ComponentId, ComponentInfo>::new();
 
-    println!("{}", PROMPT);
+    println!("{PROMPT}");
     loop {
         print!("\n> ");
         let _ = std::io::stdout().flush();
@@ -64,10 +64,10 @@ fn main() {
 
         let Some((first, rest)) = line.trim().split_once(|c: char| c.is_whitespace()) else {
             match &line.chars().next() {
-                Some('c') => println!("{}", COMPONENT_PROMPT),
-                Some('s') => println!("{}", ENTITY_PROMPT),
-                Some('q') => println!("{}", QUERY_PROMPT),
-                _ => println!("{}", PROMPT),
+                Some('c') => println!("{COMPONENT_PROMPT}"),
+                Some('s') => println!("{ENTITY_PROMPT}"),
+                Some('q') => println!("{QUERY_PROMPT}"),
+                _ => println!("{PROMPT}"),
             }
             continue;
         };
@@ -112,7 +112,7 @@ fn main() {
 
                     // Get the id for the component with the given name
                     let Some(&id) = component_names.get(name) else {
-                        println!("Component {} does not exist", name);
+                        println!("Component {name} does not exist");
                         return;
                     };
 
@@ -245,7 +245,7 @@ fn parse_term<Q: QueryData>(
     };
 
     if !matched {
-        println!("Unable to find component: {}", str);
+        println!("Unable to find component: {str}");
     }
 }
 

examples/ecs/send_and_receive_events.rs
@@ -102,7 +102,7 @@ fn send_events(mut events: EventWriter<DebugEvent>, frame_count: Res<FrameCount>
 /// Note that some events will be printed twice, because they were sent twice.
 fn debug_events(mut events: EventReader<DebugEvent>) {
     for event in events.read() {
-        println!("{:?}", event);
+        println!("{event:?}");
     }
 }
 

examples/games/stepping.rs
@@ -115,7 +115,7 @@ fn build_ui(
     for label in schedule_order {
         let schedule = schedules.get(*label).unwrap();
         text_sections.push(TextSection::new(
-            format!("{:?}\n", label),
+            format!("{label:?}\n"),
             TextStyle {
                 font: asset_server.load(FONT_BOLD),
                 color: FONT_COLOR,

commit 7507d819524bd6cdf03cd6483ffd4e29ae64fbaa

Fix formatting of `irradiance_volumes` example instructions (#15842)
# Objective

Fix minor text formatting issue introduced in #15033

Before:

<img width="1280" alt="image"
src="https://github.com/user-attachments/assets/cec4302b-a7cf-4afb-82f8-7354fa5fea00">

After:

<img width="1280" alt="image"
src="https://github.com/user-attachments/assets/ab62e58b-e563-4250-842f-90bed3a153a1">

## Solution

Add explicit linebreaks and `\` to ignore indentation on following
lines.

## Testing

`cargo run --example irradiance_volumes`

examples/3d/irradiance_volumes.rs
@@ -341,11 +341,11 @@ impl AppStatus {
         };
 
         format!(
-            "{CLICK_TO_MOVE_HELP_TEXT}
-        {voxels_help_text}
-        {irradiance_volume_help_text}
-        {rotation_help_text}
-        {switch_mesh_help_text}"
+            "{CLICK_TO_MOVE_HELP_TEXT}\n\
+            {voxels_help_text}\n\
+            {irradiance_volume_help_text}\n\
+            {rotation_help_text}\n\
+            {switch_mesh_help_text}"
         )
         .into()
     }