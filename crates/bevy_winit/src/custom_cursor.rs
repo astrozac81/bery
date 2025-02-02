bevyengine/bevy

commit e8cd12daf48f1e3f6108a83fa3bcf10e02b4c246

Automatically transform cursor hotspot user asks to flip cursor image (#17540)
# Objective

- As discussed in
https://github.com/bevyengine/bevy/issues/17276#issuecomment-2611203714,
we should transform the cursor's hotspot if the user is asking for the
image to be flipped.
- This becomes more important when a `scale` transform option exists.
It's harder for users to transform the hotspot themselves when using
`scale` because they'd need to look up the image to get its dimensions.
Instead, we let Bevy handle the hotspot transforms and make the
`hotspot` field the "original/source" hotspot.
- Refs #17276.

## Solution

- When the image needs to be transformed, also transform the hotspot. If
the image does not need to be transformed (i.e. fast path), no hotspot
transformation is applied.

## Testing

- Ran the example: `cargo run --example custom_cursor_image
--features=custom_cursor`.
- Add unit tests for the hotspot transform function.
- I also ran the example I have in my `bevy_cursor_kit` crate, which I
think is a good illustration of the reason for this PR.
- In the following videos, there is an arrow pointing up. The button
hover event fires as I move the mouse over it.
- When I press `Y`, the cursor flips. 
- In the first video, on `bevy@main` **before** this PR, notice how the
hotspot is wrong after flipping and no longer hovering the button. The
arrow head and hotspot are no longer synced.
- In the second video, on the branch of **this** PR, notice how the
hotspot gets flipped as soon as I press `Y` and the cursor arrow head is
in the correct position on the screen and still hovering the button.
Speaking back to the objective listed at the start: The user originally
defined the _source_ hotspot for the arrow. Later, they decide they want
to flip the cursor vertically: It's nice that Bevy can automatically
flip the _source_ hotspot for them at the same time it flips the
_source_ image.

First video (main):

crates/bevy_winit/src/custom_cursor.rs
@@ -17,8 +17,14 @@ pub struct CustomCursorImage {
     /// An optional texture atlas used to render the image.
     pub texture_atlas: Option<TextureAtlas>,
     /// Whether the image should be flipped along its x-axis.
+    ///
+    /// If true, the cursor's `hotspot` automatically flips along with the
+    /// image.
     pub flip_x: bool,
     /// Whether the image should be flipped along its y-axis.
+    ///
+    /// If true, the cursor's `hotspot` automatically flips along with the
+    /// image.
     pub flip_y: bool,
     /// An optional rectangle representing the region of the image to render,
     /// instead of rendering the full image. This is an easy one-off alternative
@@ -29,6 +35,10 @@ pub struct CustomCursorImage {
     pub rect: Option<URect>,
     /// X and Y coordinates of the hotspot in pixels. The hotspot must be within
     /// the image bounds.
+    ///
+    /// If you are flipping the image using `flip_x` or `flip_y`, you don't need
+    /// to adjust this field to account for the flip because it is adjusted
+    /// automatically.
     pub hotspot: (u16, u16),
 }
 
@@ -182,6 +192,28 @@ pub(crate) fn extract_and_transform_rgba_pixels(
     }
 
     Some(sub_image_data)
+}
+
+/// Transforms the `hotspot` coordinates based on whether the image is flipped
+/// or not. The `rect` is used to determine the image's dimensions.
+pub(crate) fn transform_hotspot(
+    hotspot: (u16, u16),
+    flip_x: bool,
+    flip_y: bool,
+    rect: Rect,
+) -> (u16, u16) {
+    let hotspot_x = hotspot.0 as f32;
+    let hotspot_y = hotspot.1 as f32;
+    let (width, height) = (rect.width(), rect.height());
+
+    let hotspot_x = if flip_x { width - hotspot_x } else { hotspot_x };
+    let hotspot_y = if flip_y {
+        height - hotspot_y
+    } else {
+        hotspot_y
+    };
+
+    (hotspot_x as u16, hotspot_y as u16)
 }
 
 #[cfg(test)]
@@ -542,4 +574,48 @@ mod tests {
             0, 255, 255, 255, // Cyan
         ]
     );
+
+    #[test]
+    fn test_transform_hotspot_no_flip() {
+        let hotspot = (10, 20);
+        let rect = Rect {
+            min: Vec2::ZERO,
+            max: Vec2::new(100.0, 200.0),
+        };
+        let transformed = transform_hotspot(hotspot, false, false, rect);
+        assert_eq!(transformed, (10, 20));
+    }
+
+    #[test]
+    fn test_transform_hotspot_flip_x() {
+        let hotspot = (10, 20);
+        let rect = Rect {
+            min: Vec2::ZERO,
+            max: Vec2::new(100.0, 200.0),
+        };
+        let transformed = transform_hotspot(hotspot, true, false, rect);
+        assert_eq!(transformed, (90, 20));
+    }
+
+    #[test]
+    fn test_transform_hotspot_flip_y() {
+        let hotspot = (10, 20);
+        let rect = Rect {
+            min: Vec2::ZERO,
+            max: Vec2::new(100.0, 200.0),
+        };
+        let transformed = transform_hotspot(hotspot, false, true, rect);
+        assert_eq!(transformed, (10, 180));
+    }
+
+    #[test]
+    fn test_transform_hotspot_flip_both() {
+        let hotspot = (10, 20);
+        let rect = Rect {
+            min: Vec2::ZERO,
+            max: Vec2::new(100.0, 200.0),
+        };
+        let transformed = transform_hotspot(hotspot, true, true, rect);
+        assert_eq!(transformed, (90, 180));
+    }
 }

crates/bevy_winit/src/cursor.rs
@@ -8,7 +8,7 @@ use crate::{
 use crate::{
     custom_cursor::{
         calculate_effective_rect, extract_and_transform_rgba_pixels, extract_rgba_pixels,
-        CustomCursorPlugin,
+        transform_hotspot, CustomCursorPlugin,
     },
     state::{CustomCursorCache, CustomCursorCacheKey},
     WinitCustomCursor,
@@ -124,10 +124,13 @@ fn update_cursors(
                     let (rect, needs_sub_image) =
                         calculate_effective_rect(&texture_atlases, image, texture_atlas, rect);
 
-                    let maybe_rgba = if *flip_x || *flip_y || needs_sub_image {
-                        extract_and_transform_rgba_pixels(image, *flip_x, *flip_y, rect)
+                    let (maybe_rgba, hotspot) = if *flip_x || *flip_y || needs_sub_image {
+                        (
+                            extract_and_transform_rgba_pixels(image, *flip_x, *flip_y, rect),
+                            transform_hotspot(*hotspot, *flip_x, *flip_y, rect),
+                        )
                     } else {
-                        extract_rgba_pixels(image)
+                        (extract_rgba_pixels(image), *hotspot)
                     };
 
                     let Some(rgba) = maybe_rgba else {

astrozac81/bery

commit 6ae3f98a0b8ee1cbe0320d4c627134f38bf127e7

Fix cursor hotspot out of bounds when flipping (#17571)
# Objective

- Fix off by one error introduced in
https://github.com/bevyengine/bevy/pull/17540 causing:

```
Cursor image StrongHandle<Image>{ id: Index(AssetIndex { generation: 0, index: 3 }), path: Some(cursors/kenney_crosshairPack/Tilesheet/crosshairs_tilesheet_white.png) } is invalid: The specified hotspot (64, 64) is outside the image bounds (64x64).
```

- First PR commit and run shows the bug:
https://github.com/bevyengine/bevy/actions/runs/13009405866/job/36283507530?pr=17571
- Second PR commit fixes it.

## Solution

- Hotspot coordinates are 0-indexed, so we need to subtract 1 from the
width and height.

## Testing

- Fix the tests which included the off-by-one error in their expected
values.
- Consolidate the tests into a single test for brevity.
- Test round trip transform to ensure we can "undo" to get back to the
original value.
- Add a specific bounds test.
- Ran the example again and observed there are no more error logs:
`cargo run --example custom_cursor_image --features=custom_cursor`.

crates/bevy_winit/src/custom_cursor.rs
@@ -204,11 +204,16 @@ pub(crate) fn transform_hotspot(
 ) -> (u16, u16) {
     let hotspot_x = hotspot.0 as f32;
     let hotspot_y = hotspot.1 as f32;
+
     let (width, height) = (rect.width(), rect.height());
 
-    let hotspot_x = if flip_x { width - hotspot_x } else { hotspot_x };
+    let hotspot_x = if flip_x {
+        (width - 1.0).max(0.0) - hotspot_x
+    } else {
+        hotspot_x
+    };
     let hotspot_y = if flip_y {
-        height - hotspot_y
+        (height - 1.0).max(0.0) - hotspot_y
     } else {
         hotspot_y
     };
@@ -576,46 +581,26 @@ mod tests {
     );
 
     #[test]
-    fn test_transform_hotspot_no_flip() {
-        let hotspot = (10, 20);
-        let rect = Rect {
-            min: Vec2::ZERO,
-            max: Vec2::new(100.0, 200.0),
-        };
-        let transformed = transform_hotspot(hotspot, false, false, rect);
-        assert_eq!(transformed, (10, 20));
-    }
+    fn test_transform_hotspot() {
+        fn test(hotspot: (u16, u16), flip_x: bool, flip_y: bool, rect: Rect, expected: (u16, u16)) {
+            let transformed = transform_hotspot(hotspot, flip_x, flip_y, rect);
+            assert_eq!(transformed, expected);
 
-    #[test]
-    fn test_transform_hotspot_flip_x() {
-        let hotspot = (10, 20);
-        let rect = Rect {
-            min: Vec2::ZERO,
-            max: Vec2::new(100.0, 200.0),
-        };
-        let transformed = transform_hotspot(hotspot, true, false, rect);
-        assert_eq!(transformed, (90, 20));
-    }
+            // Round-trip test: Applying the same transformation again should
+            // reverse it.
+            let transformed = transform_hotspot(transformed, flip_x, flip_y, rect);
+            assert_eq!(transformed, hotspot);
+        }
 
-    #[test]
-    fn test_transform_hotspot_flip_y() {
-        let hotspot = (10, 20);
         let rect = Rect {
             min: Vec2::ZERO,
             max: Vec2::new(100.0, 200.0),
         };
-        let transformed = transform_hotspot(hotspot, false, true, rect);
-        assert_eq!(transformed, (10, 180));
-    }
 
-    #[test]
-    fn test_transform_hotspot_flip_both() {
-        let hotspot = (10, 20);
-        let rect = Rect {
-            min: Vec2::ZERO,
-            max: Vec2::new(100.0, 200.0),
-        };
-        let transformed = transform_hotspot(hotspot, true, true, rect);
-        assert_eq!(transformed, (90, 180));
+        test((10, 20), false, false, rect, (10, 20)); // no flip
+        test((10, 20), true, false, rect, (89, 20)); // flip X
+        test((10, 20), false, true, rect, (10, 179)); // flip Y
+        test((10, 20), true, true, rect, (89, 179)); // flip both
+        test((0, 0), true, true, rect, (99, 199)); // flip both (bounds check)
     }
 }